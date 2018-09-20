14a83acdb66817f551bc3ac7005268e6ca79cfcd
gwladysledocq <gwladys.ledocq@gmail.com>
2018-09-20 17:33:16 +0200

    Add files via upload
    Tuto Node.js OpenClassRoom

---
+++ Front/Node.js/Formation_OpenClassRoom/Mon premier programme/serveur_page.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if (page == '/') {
+        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    }
+    else if (page == '/sous-sol') {
+        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+    }
+    else if (page == '/etage/1/chambre') {
+        res.write('Hé ho, c\'est privé ici !');
+    } else {
+		res.write('Erreur 404 ! Cet endroit n\'existe pas !');
+	}
+    res.end();
+});
+server.listen(8080);
---
+++ Front/Node.js/Formation_OpenClassRoom/Mon_application_avec_Express/app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var favicon = require('serve-favicon'); // Charge le middleware de favicon
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
+});
+
+// Routes simples
+
+app.get('/sous-sol', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+});
+
+app.get('/etage/1/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hé ho, c\'est privé ici !');
+});
+
+// Routes dynamiques avec template
+
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.render('chambre.ejs', {etage: req.params.etagenum});
+});
+
+app.get('/compter/:nombre', function(req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
+});
+
+// Les middlewares
+
+app.use(morgan('combined')) // Active le middleware de logging
+.use(express.static(__dirname + '/public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+.use(favicon(__dirname + '/public/favicon.ico')) // Active la favicon indiquée
+.use(function(req, res){ // Répond enfin
+    res.send('Hello');
+});
+
+app.listen(8080);
+
+// Erreur
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(8181);
